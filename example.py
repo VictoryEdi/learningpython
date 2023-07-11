456c1f650b6ef2330009b11f05448b0e9296e9bc
VictoryEdi <johnsonedikan942@gmail.com>
2023-07-11 07:07:06 +0100

    examples of datatypes in python

---
+++ example.py
@@ -0,0 +1,21 @@
+#string: johmson, H. "johmson". "h" 123
+print("johnson")
+
+#int: 1, 12, 
+print(12)
+
+#boolean
+#floating: 2.1
+print(1.2)
+
+#list
+family = ["johnson", "edima", 20, ["mother", 50,3.1]]
+
+print(family)
+
+print(2 * 4)
+
+a = 2
+b = 3
+total = a + b
+print(total)
